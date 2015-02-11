e68295a2709e7a5cab194146e60584b51cacd9c9
Lorenzo <lorenzo@smugbit.com>
2015-02-11 14:42:54 -0500

    adding sidebar nav

--- client/templates/dashboard/myTrackers.js
+++ client/templates/dashboard/myTrackers.js
@@ -68,81 +68,58 @@ Template.myTrackers.helpers({
   },
 
   calOptions: function() {
-  	var date = new Date();
+  	var checks = Checkins.find();
+
+    var myEvents = new Array();
+
+    // signify event creation somehow
+    //myEvents.push({start: item.createdAt,
+    //                title: item.name,
+    //                color: '#ffffff'});   
+
+    if(checks)
+    {
+      checks.forEach(function(checkin)
+      {
+        var user = Meteor.users.findOne(checkin.creatorId);
+        myEvents.push({start: checkin.createdAt,
+                    title: user.profile.name});     
+      });
+    }
+
+    //console.log(myEvents);
+
+    var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
         return {
-        	//height: 200,
-        	//contentHeight: 200,
-        	id: 'myCalendar',
-        	aspectRatio: 1.0,
-        	//fixedWeekCount: false,
-        	theme: true,
-	        //header: {
-	        //    left: 'prev,next today',
-	        //    center: 'title',
-	        //    right: 'month,agendaWeek,agendaDay'
-	        //},
-	        //editable: true,
-	        dayRender: function (date, cell) {
-        		//cell.css("background-color", "red");
-    		},
-    		eventRender: function(event, element) { 
-            	//element.find('.fc-title').append("<br/>" + event.description); 
-        	},
-        	//eventLimit: 1,
-	        events: function (start, end, tz, callback) {
-                callback([{
-                        title: new Date().toGMTString(),
-                        start: new Date()
-                    }]);
-            },
+          //height: 200,
+          //contentHeight: 200,
+          id: 'myCalendar',
+          aspectRatio: 1.0,
+          //fixedWeekCount: false,
+          theme: true,
+          //header: {
+          //    left: 'prev,next today',
+          //    center: 'title',
+          //    right: 'month,agendaWeek,agendaDay'
+          //},
+          //editable: true,
+          dayRender: function (date, cell) {
+            //cell.css("background-color", "red");
+        },
+        eventRender: function(event, element) { 
+              //element.find('.fc-title').append("<br/>" + event.description); 
+          },
+          eventLimit: 2,
+          
             eventMouseover: function(event, jsEvent, view) {
             if (view.name !== 'agendaDay') {
                 $(jsEvent.target).attr('title', event.title);
-            	}
-        	}/*,
-        	events: [
-            {
-            title: 'All Day Event',
-            start: new Date(y, m, 1)},
- 	     	//backgroundColor: '#D5D8DC'},
-        //{
-        //    title: 'Long Event',
-        //    start: new Date(y, m, d - 5),
-        //    end: new Date(y, m, d - 2)},
-        {
-            id: 999,
-            title: 'Repeating Event',
-            start: new Date(y, m, d - 3, 16, 0),
-            allDay: false,
-        	color: '#990000'},
-        {
-            id: 999,
-            title: 'Repeating Event',
-            start: new Date(y, m, d + 4, 16, 0),
-            allDay: false},
-        {
-            title: 'Meeting',
-            start: new Date(y, m, d, 10, 30),
-            allDay: false},
-        {
-            title: 'Lunch',
-            start: new Date(y, m, d, 12, 0),
-            end: new Date(y, m, d, 14, 0),
-            allDay: false},
-        {
-            title: 'Birthday Party',
-            start: new Date(y, m, d + 1, 19, 0),
-            end: new Date(y, m, d + 1, 22, 30),
-            allDay: false},
-        {
-            title: 'Click for Google',
-            start: new Date(y, m, 28),
-            end: new Date(y, m, 29),
-            url: 'http://google.com/'}
-        ]*/
+              }
+          },
+          events: myEvents
             //defaultView: 'basicWeek'
         };
     }
